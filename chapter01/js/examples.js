34e0b39be0ff91c9e18035e3d904ae10514d389b
pleeplop <12030781+pleeplop@users.noreply.github.com>
2019-08-08 21:45:39 +0200

    add chapter01

---
+++ chapter01/js/examples.js
@@ -0,0 +1,14 @@
+/**
+ * Some
+ * multi-line
+ * comments
+ * 
+ */
+console.log("Hello from JavaScript!");
+console.log("Let's do some math");
+console.log(4 + 7);
+console.log(7 / 0);
+console.log("Goodbye");
+
+console.log("6 + 3");
+console.log(6 + 3);
